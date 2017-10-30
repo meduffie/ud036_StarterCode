7a81a04b83e4bb888b3bb46f30563dfad44ce1d4
Austin Duffie <meduffie@users.noreply.github.com>
2017-10-30 18:18:00 -0500

    Changed for code review

--- media.py
+++ media.py
@@ -1,6 +1,12 @@
 class Movie():
+    """ This class defines the Movie type, which stores information
+        like titles, youtube trailers, etc about movies """
+    
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """ This constructor takes the given params when objects of
+            the movie type are created and sets them to the proper
+            instance variables """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
